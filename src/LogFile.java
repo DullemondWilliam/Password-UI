fbfe152c4d3f93c3d1daa934e7b111a8b039051b
William Dullemond <Dullemond.William@gmail.com>
2017-03-24 18:18:11 -0400

    Log File Writer
    - Untested

---
+++ src/LogFile.java
@@ -0,0 +1,72 @@
+import java.util.Date;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class LogFile 
+{
+	private static String SITE   = "applet";
+	private static String SCHEME = "DaBest"; 
+
+	private String m_fileName;
+	
+
+	public LogFile( String fileName )
+	{
+		m_fileName = fileName;
+
+		try
+		{
+			String header = "\"time\",\"site\",\"user\",\"scheme\","
+					      + "\"mode\",\"event\",\"data\"";
+
+			FileWriter writer = new FileWriter( m_fileName );
+			writer.write( header );
+			writer.flush();
+			writer.close();
+		}
+		catch( IOException e )
+			System.out.println( e );
+	}
+	
+	public int recordData( String user, String mode, String event, String data )
+	{
+		String record = "";
+		record += '"' + getTime() + "\",";
+		record += '"' + SITE      + "\",";
+		record += '"' + user   	  + "\",";
+		record += '"' + SCHEME    + "\",";
+		record += '"' + mode      + "\",";
+		record += '"' + event     + "\",";
+		record += '"' + data      + "\",";
+		
+		try
+		{
+			FileWriter writer = new FileWriter( m_fileName );
+			writer.write( record );
+			writer.flush();
+			writer.close();
+		}
+		catch( IOException e )
+		{
+			System.out.println( e );
+			return -1;
+		}
+
+		return 1;
+	}
+	
+	private String getTime()
+	{
+		Date date = new Date();
+		String time = "";
+		time += date.getMonth() + "/";
+		time += date.getDate()  + "/";
+		time += date.getYear()  + " ";
+		
+		time += date.getHours()   + ":";
+		time += date.getMinutes() + ":";
+		time += date.getSeconds();
+		
+		return time;
+	}
+}
